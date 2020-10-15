2620c8cf2d8dd1335a5b1d8148984e67c77e49e2
kwu3 <kwu3@villanova.edu>
2020-10-14 23:59:37 -0400

    Added working carousel with video player

---
+++ src/components/FunCarousel.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel';
+import ReactPlayer from 'react-player';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import './styles.css';
+
+const FunCarousel = () => {
+  return (
+    <CarouselProvider
+      naturalSlideWidth={1280}
+      naturalSlideHeight={720}
+      totalSlides={5}
+    >
+      <Slider>
+        <Slide index={0}>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=cnXapYkboRQ'
+              width='100%'
+              height='100%'
+              volume='0.1'
+              light
+            />
+          </div>
+        </Slide>
+        <Slide index={1}>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=cnXapYkboRQ'
+              width='100%'
+              height='100%'
+              volume='0.1'
+              light
+            />
+          </div>
+        </Slide>
+        <Slide index={2}>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=cnXapYkboRQ'
+              width='100%'
+              height='100%'
+              volume='0.1'
+              light
+            />
+          </div>
+        </Slide>
+        <Slide index={3}>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=cnXapYkboRQ'
+              width='100%'
+              height='100%'
+              volume='0.1'
+              light
+            />
+          </div>
+        </Slide>
+        <Slide index={4}>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url='https://www.youtube.com/watch?v=cnXapYkboRQ'
+              width='100%'
+              height='100%'
+              volume='0.1'
+              light
+            />
+          </div>
+        </Slide>
+      </Slider>
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
+  );
+};
+
+export default FunCarousel;
